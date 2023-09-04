aba83c555fae474df870dcbd2d81dacecb6d1da0
NovakLBUT <novak.l@fce.vutbr.cz>
2023-09-04 22:12:43 +0200

    New Class for GC sw

---
+++ GC_Expansion.py
@@ -0,0 +1,47 @@
+import numpy as np
+from scipy import stats as sc
+
+
+class GramCharlier(object):
+    def __init__(self, moments):
+        self.moments = moments
+
+    def normalized_hermite(self, N):
+        plist = [None] * N
+        plist[0] = np.poly1d(1)
+        for n in range(1, N):
+            plist[n] = (-plist[n - 1].deriv() + np.poly1d([1, 0]) * plist[n - 1]) / np.sqrt(n)
+        return plist
+
+    def standardize_sample(self, x):
+        s = (x - self.moments[0]) / np.sqrt(self.moments[1])
+        return s
+
+    def pdf(self, x):
+        m1, m2, m3, m4 = self.moments
+        GCexp = np.poly1d(1)
+        s = self.standardize_sample(x)
+        Hp = self.normalized_hermite(5)
+
+        C3 = m3 / np.sqrt(6.0)
+        C4 = (m4 - 3) / np.sqrt(24.0)
+
+        GCexp = GCexp + C3 * Hp[3] + C4 * Hp[4]
+
+        p = GCexp(s) * np.exp(-s * s / 2.0) / np.sqrt(2 * np.pi) / np.sqrt(m2)
+
+        return p
+
+    def cdf(self, x):
+        m1, m2, m3, m4 = self.moments
+        GCexp = np.poly1d(1)
+        s = self.standardize_sample(x)
+        Hp = self.normalized_hermite(5)
+
+        C3 = -m3 / (np.sqrt(2) * 3)
+        C4 = -(m4 - 3) / (np.sqrt(6) * 4)
+
+        GCexp = GCexp + C3 * Hp[2] + C4 * Hp[3]
+        p = sc.norm.cdf(s) + GCexp(s) * np.exp(-s * s / 2.0) / np.sqrt(2 * np.pi) / np.sqrt(m2) - np.exp(
+            -s * s / 2.0) / np.sqrt(2 * np.pi) / np.sqrt(m2)
+        return p
